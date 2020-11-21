cd67e0dc476659ecadd8d9e9879a96f0001da8ec
James Sandford <jamesmsandford@googlemail.com>
2020-11-21 15:08:49 +0000

    Basic web and websocket server

--- santaServer/santaServer.py
+++ santaServer/santaServer.py
@@ -4,6 +4,13 @@ import json
 from os import path, listdir
 from random import choice as randChoice
 import logging
+import pickle
+
+import tornado.websocket
+from tornado.web import RequestHandler
+from tornado.ioloop import IOLoop
+
+PORT = 80
 
 logFormat = '%(asctime)s %(levelname)s %(pathname)s %(lineno)s %(message)s'
 logLevel = logging.ERROR
@@ -17,6 +24,7 @@ else:
     logging.basicConfig(format=logFormat,
                         level=logLevel)
 dataDir = "/etc/data"
+stateFile = f"{dataDir}state.txt"
 infoName = "info.json"
 picExts = [".jpg", ".jpeg", ".png"]
 wrappedNames = [f"wrapped{e}" for e in picExts]
@@ -27,7 +35,7 @@ def intersection(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 
-class Application():
+class Game():
     def __init__(self):
         def wrappedName(theseNames, p):
             names = intersection(theseNames, wrappedNames)
@@ -47,17 +55,28 @@ class Application():
 
         self.presents = {}
         self.players = {}
+        self.sockets = []
+        self.nextPlayer = None
+
+        if path.isfile(stateFile):
+            self.loadState()
 
         dirCont = listdir(dataDir)
 
         for name in dirCont:
             fullPath = path.join(dataDir, name)
             if path.isdir(fullPath):
-                self.presents[name] = {
-                    "path": fullPath
-                }
+                # if not in loaded state
+                if name not in self.presents:
+                    self.presents[name] = {
+                        "path": fullPath
+                    }
 
         for p in self.presents:
+            if p in self.players:
+                # Was in loaded state
+                continue
+
             present = self.presents[p]
             pDir = listdir(present["path"])
             if infoName not in pDir:
@@ -78,8 +97,11 @@ class Application():
     def assignOwner(self, present, owner):
         oldOwner = self.presents[present]["owner"]
         self.presents[present]["owner"] = owner
+        self.nextPlayer = oldOwner
 
-        return {oldOwner: self.players[oldOwner]}
+        self.saveState()
+
+        return {'oldOwner': self.players[oldOwner]}
 
     def playersFromNumbers(self, numList):
         return {p: self.players[p] for p in self.players
@@ -100,12 +122,104 @@ class Application():
     def randomPlayerWithoutPres(self):
         if len(self.playersWithoutPresents) > 0:
             player = randChoice(list(self.playersWithoutPresents))
-            return {player: self.players[player]}
+            return player
         return None
 
+    def getNextPlayer(self):
+        if self.nextPlayer is not None:
+            return self.nextPlayer
+        self.nextPlayer = self.randomPlayerWithoutPres()
+        self.saveState()
+
+        return self.nextPlayer
+
+    def saveState(self):
+        state = {
+            "players": self.players,
+            "presents": self.presents,
+            "nextPlayer": self.nextPlayer
+        }
+        with open(stateFile, "wb") as f:
+            pickle.dump(state, f)
+
+    def loadState(self):
+        with open(stateFile, "rb") as f:
+            d = pickle.load(f)
+            self.players = d["players"]
+            self.presents = d["presents"]
+            self.nextPlayer = d["nextPlayer"]
+
+    def wsSendState(self):
+        state = {
+            "playersPresents": self.playersWithPresents,
+            "playersNoPresents": self.playersWithoutPresents,
+            "presents": self.presents,
+            "nextPlayer": self.nextPlayer
+        }
+        for s in self.sockets:
+            s.write_message(state)
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def initialize(self, game):
+        self.game = game
+
+    def open(self):
+        self.game.sockets.append(self)
+        print('new connection')
+
+    def on_close(self):
+        self.game.sockets.remove(self)
+        print('connection closed')
+
+
+class BaseHandler(RequestHandler):
+    def initialize(self, game):
+        self.game = game
+
+
+class NextPlayerHandler(BaseHandler):
+    def get(self):
+        self.game.wsSendState()
+        self.write(self.game.getNextPlayer())
+
+
+class SetPresentHandler(BaseHandler):
+    def put(self, present):
+        oldOwner = self.game.assignOwner(present, self.request.body)
+        self.write(oldOwner)
+
+
+class Application():
     def run(self):
-        print(self.presents)
-        print(self.players)
-        print(self.playersWithPresents)
-        print(self.playersWithoutPresents)
-        print(self.randomPlayerWithoutPres())
+        self.game = Game()
+
+        print(self.game.presents)
+        print(self.game.players)
+        print(self.game.playersWithPresents)
+        print(self.game.playersWithoutPresents)
+        print(self.game.randomPlayerWithoutPres())
+
+        handlerArgs = {"game": self.game}
+
+        settings = {
+            "static_path": dataDir,
+        }
+
+        server = tornado.web.Application([
+            (r'/ws/?', WSHandler, handlerArgs),
+            (r'/nextPlayer/?', NextPlayerHandler, handlerArgs),
+            (r'/pres/([^/]*)/?', SetPresentHandler, handlerArgs),
+            (r"/(index\.html)", tornado.web.StaticFileHandler,
+             dict(path=settings['static_path'])),
+            (r"/(index\.css)", tornado.web.StaticFileHandler,
+             dict(path=settings['static_path'])),
+            (r"/(bootstrap\.min\.css)", tornado.web.StaticFileHandler,
+             dict(path=settings['static_path'])),
+            (r"/(santa\.js)", tornado.web.StaticFileHandler,
+             dict(path=settings['static_path'])),
+        ], **settings)
+
+        server.listen(PORT)
+
+        IOLoop.current().start()
